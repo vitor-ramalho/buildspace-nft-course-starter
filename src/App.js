ce582803906b4eca670b95ff6f3a64a15f833e74
{"nickname": "JSON"} <39497088+vitor-ramalho@users.noreply.github.com>
2023-03-16 15:16:12 -0300

    Project complete
    Complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,199 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+import myEpicNft from './utils/MyEpicNft.json';
+import { networks } from './utils/networks';
+
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
-
+const CONTRACT_ADDRESS = "0x6290FBAC2DD385DF6769D0731b0F16eE886b8de6";
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [network, setNetwork] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    /**
+     * First make sure we have access to window.ethereum
+     */
+
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!")
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum)
+    }
+
+    /*
+    * Check if we're authorized to access the user's wallet
+    */
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /*
+  * User can have multiple authorized accounts, we grab the first one if its there!
+  */
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      setupEventListener();
+    } else {
+      console.log("No authorized account found");
+    }
+
+    const chainId = await ethereum.request({ method: 'eth_chainId' });
+    setNetwork(networks[chainId]);
+    ethereum.on('chainChanged', handleChainChanged)
+    function handleChainChanged(_chainId) {
+      window.location.reload();
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return
+      }
+
+      /*
+     * Fancy method to request access to account.
+     */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      setupEventListener();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+
+        const signer = provider.getSigner();
+
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://mumbai.polygonscan.com/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const switchNetwork = async () => {
+    if (window.ethereum) {
+      try {
+        // Try to switch to the Mumbai testnet
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x13881' }], // Check networks.js for hexadecimal network ids
+        });
+      } catch (error) {
+        if (error.code === 4902) {
+          try {
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: '0x13881',
+                  chainName: 'Polygon Mumbai Testnet',
+                  rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+                  nativeCurrency: {
+                    name: "Mumbai Matic",
+                    symbol: "MATIC",
+                    decimals: 18
+                  },
+                  blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+                },
+              ],
+            })
+          } catch (error) {
+            console.log(error)
+          }
+        }
+        console.log(error)
+      }
+    } else {
+      alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html')
+    }
+  }
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
       Connect to Wallet
     </button>
   );
+
+  const renderSwitchNetworkContainer = () => (
+    <div className="connect-wallet-container">
+      <p>Please connect to Polygon Mumbai Testnet</p>
+      <button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+    </div>
+  )
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="App">
@@ -24,7 +203,20 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {
+            network !== 'Polygon Mumbai Testnet' ? (
+              renderSwitchNetworkContainer()
+            ) : (
+              currentAccount === "" ? (
+                renderNotConnectedContainer()
+              ) : (
+                <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+                  Mint NFT
+                </button>
+              )
+            )
+
+          }
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
